b3e5519fc2dcd109354c027216515bfe2e557cf7
Yugang Cao <yugang.cao12@gmail.com>
2024-04-16 16:53:08 +0800

    feat: docs

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ export * from './configs'
 export * from './types'
 export * from './utils'
 import { createEslintConfig } from './factory'
-export { createEslintConfig }
+export default createEslintConfig
+
+export {
+  createEslintConfig
+}
